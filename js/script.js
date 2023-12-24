ef0ab10e192179fc5f4007963155ee1af5e86adc
Dmitrii23748 <chunosov.dmitrij@bk.ru>
2023-12-24 14:09:11 +0300

    add tabs and padding sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(currentBtn) {
+  currentBtn.addEventListener("click", function () {
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.getElementById(tabId);
+
+    if (!currentBtn.classList.contains("active")) {
+      tabsBtn.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabsItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
+
 const swiper = new Swiper('.reviews-swiper', {
     loop: true,
     slidesPerView: 3,
